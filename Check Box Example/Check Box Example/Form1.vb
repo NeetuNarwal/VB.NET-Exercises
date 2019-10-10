1d17514c03abcb8fb49515ec2700d713ca25aacf
Dr. Neetu Narwal <54794403+NeetuNarwal@users.noreply.github.com>
2019-10-10 19:19:37 +0530

    Added feature in Click event

--- Check Box Example/Check Box Example/Form1.vb
+++ Check Box Example/Check Box Example/Form1.vb
@@ -5,10 +5,14 @@
 
         If CheckBox1.Checked Then
             MsgBox("Hello!")
+            MessageBox.Show("You Clicked on Checkbox " & CheckBox1.text)
+            CheckBox1.ForeColor=Color.Red
+            
         End If
 
         If CheckBox2.Checked Then
             Me.BackColor = Color.Brown
+             MessageBox.Show("You Clicked on Checkbox " & CheckBox2.text)
         End If
     End Sub
 End Class
