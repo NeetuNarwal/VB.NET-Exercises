7ba9a0b424c087d0ff1a85a1ac82baecfb5aaed1
Dr. Neetu Narwal <54794403+NeetuNarwal@users.noreply.github.com>
2020-10-22 14:48:57 +0530

    Update Form1.vb

--- array bubble sorting/array bubble sorting/Form1.vb
+++ array bubble sorting/array bubble sorting/Form1.vb
@@ -1,4 +1,19 @@
 ﻿Public Class Form1
-
+Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim ar(5) As Integer
+        Dim i, j, temp As Integer
+        For i = 0 To 5
+            ar(i) = InputBox("Enter element in array")
+        Next
+        For i = 0 To 4
+            For j = i To 5
+                If ar(i) > ar(j) Then
+                    temp = ar(i)
+                    ar(i) = ar(j)
+                    ar(j) = temp
+                End If
+            Next
+        Next
+    End Sub
 
 End Class
